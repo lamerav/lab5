a137567ea95b8088701ce9d3ff92b7b8bb2863f1
lamerav <89978103+lamerav@users.noreply.github.com>
2021-10-26 17:36:34 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <vector>
+#import <random>
+
+using namespace std;
+
+
+void oddEvensort(vector<int> &m, char s)
+{
+    if (s == '-')
+    {
+        for (int i = 0; i < m.size(); i++) {
+            if (i % 2 == 0) {
+                for (int j = 2; j < m.size(); j += 2) {
+                    if (m[j] > m[j - 1]) {
+                        swap(m[j - 1], m[j]);
+                    }
+                }
+            } else {
+                for (int j = 1; j < m.size(); j += 2) {
+                    if (m[j] > m[j - 1]) {
+                        swap(m[j - 1], m[j]);
+                    }
+
+                }
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < m.size(); i++) {
+            if (i % 2 == 0) {
+                for (int j = 2; j < m.size(); j += 2) {
+                    if (m[j] < m[j - 1]) {
+                        swap(m[j - 1], m[j]);
+                    }
+                }
+            } else {
+                for (int j = 1; j < m.size(); j += 2) {
+                    if (m[j] < m[j - 1]) {
+                        swap(m[j - 1], m[j]);
+                    }
+
+                }
+            }
+        }
+    }
+
+}
+
+
+void print(vector<int> const& vec){
+    for (int const& v: vec)
+        cout << v << " ";
+    cout << endl;
+}
+
+vector<int> operator + (vector<int> &vec1,vector<int> &vec2){
+    if (vec1.size() < vec2.size()){
+        for (int i = vec1.size() - 1 ; i < vec2.size(); i++){
+            vec1.push_back(0);
+        }
+    }
+    else if (vec1.size() > vec2.size()){
+        for (int i = vec2.size() - 1 ; i < vec1.size(); i++){
+            vec2.push_back(0);
+        }
+    }
+    vector<int> tmpvec;
+    for (int i = 0; i < vec1.size(); i++){
+        tmpvec.push_back(vec1[i] + vec2[i]);
+    }
+    return tmpvec;
+}
+
+
+int main()
+{
+    random_device rd;
+    cout << "Task 1" << endl;
+    /*
+    int len;
+    char s;
+    cout << "Enter length of vec: " << endl;
+    cin >> len;
+    cout << "Enter - if vector of vec is minus" << endl;
+    cin >> s;
+    cout << endl;
+
+    vector<int> vec(len);
+    for (int i = 0; i < len; i++){
+        vec[i] = rd() % 1000;
+    }
+
+
+
+    oddEvensort(vec, s);
+
+    for (int n: vec){
+        cout << n << " ";
+    }
+    */
+    cout << "Task 2" << endl;
+
+    int len1, len2;
+
+    cout << "Enter length of first vec" << endl;
+    cin >> len1;
+    cout << endl;
+    vector<int> vec1(len1);
+    for (int i = 0; i < len1; i++){
+        vec1[i] = rd() % 1000;
+    }
+
+    cout << "Enter length of second vec" << endl;
+    cin >> len2;
+    cout << endl;
+    vector<int> vec2(len2);
+    for (int i = 0; i < len2; i++){
+        vec2[i] = rd() % 1000;
+    }
+    print(vec1);
+    print(vec2);
+    vector<int> anotherVec(vec1 + vec2);
+    anotherVec.erase(anotherVec.end() - 1);
+    print(anotherVec);
+
+
+    cout << "Task 3" << endl;
+    /*
+    int len3, a;
+    cout << "Enter length vector: " << endl;
+    cin >> len3;
+    cout << endl;
+    vector<int> vec3;
+    cout << "Enter values of vector:" << endl;
+    for (int i = 0; i < len3; i++){
+        cin >> a;
+        vec3.push_back(a);
+    }
+    cout << "Your vec before sort: " << endl;
+    print(vec3);
+    cout << "Enter - if vector of youe vec is minus" << endl;
+    char qs;
+    cin >> qs;
+    oddEvensort(vec3, qs);
+    cout << "Your vec after sort: " << endl;
+    print(vec3);
+
+
+    cout << "Enter length of your vectors:" << endl;
+    int len4, c, b;
+    cin >> len4;
+    cout << "Enter your first vector:" << endl;
+    vector<int> vec4;
+    for (int i = 0; i < len4; i++) {
+        cin >> c;
+        vec4.push_back(c);
+    }
+    cout << "Enter your second vector:" << endl;
+    vector<int> vec5;
+    for (int i = 0; i < len4; i++) {
+        cin >> c;
+        vec5.push_back(c);
+    }
+    cout << "Sum elements of yours vectors is vector: " << endl;
+    print(vec4 + vec5);
+    */
+    return 0;
+}
